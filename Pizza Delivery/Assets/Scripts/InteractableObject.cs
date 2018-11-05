24a4eb787c0e92d5df094ce52fa6ec8ec821d9c1
DocQuantum <shchurgood@gmail.com>
2018-11-04 21:04:19 -0600

    Added player related files from environment dev
    Manually merged environment and level dev branches, organized player related assets into folders.

---
+++ Pizza Delivery/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+    private bool isPaused;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isPaused = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Pause(bool tf)
+    {
+        isPaused = tf;
+    }
+}
